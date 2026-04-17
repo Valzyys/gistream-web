5370c928b2fb5c9c997a43eea33083887dd30e3d
Valzz <jkt48connect@gmail.com>
2026-04-17 23:30:13 +0700

    Update AppSidebar.tsx

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -44,20 +44,12 @@ const navItems: NavItem[] = [
   {
     name: "Forms",
     icon: <ListIcon />,
-    subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
+    path: "/news",
   },
   {
     name: "Tables",
     icon: <TableIcon />,
-    subItems: [{ name: "Basic Tables", path: "/basic-tables", pro: false }],
-  },
-  {
-    name: "Pages",
-    icon: <PageIcon />,
-    subItems: [
-      { name: "Blank Page", path: "/blank", pro: false },
-      { name: "404 Error", path: "/error-404", pro: false },
-    ],
+    path: "/live",
   },
 ];
 
